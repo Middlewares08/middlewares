e722f19674c80ccf951081dc40b2cc4d87877488
ricomanabat <rico.manabat@supportcommunity.com>
2026-04-15 11:58:38 +0800

    Fix the linking of pages and add the reac-router-dom

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react'
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import ServicesSection from './components/ServicesSection';
-import AboutUsSection from './components/AboutUsSection';
-import PortfolioSection from './components/PortfolioSection';
-import ContactUsSection from './components/ContactUsSection';
-import FooterSection from './components/FooterSection';
+import Navbar from '../components/Navbar'
+import HeroSection from '../components/HeroSection'
+import ServicesSection from '../components/ServicesSection';
+import AboutUsSection from '../components/AboutUsSection';
+import PortfolioSection from '../components/PortfolioSection';
+import ContactUsSection from '../components/ContactUsSection';
+import FooterSection from '../components/FooterSection';
 
 
 function LandingPage() {
